7f1f455e446cf993ce9c603a9d4acefd1cfe6d58
Benedikt Linse <benedikt.linse@gmail.com>
2013-11-08 20:21:22 +0100

    cleanup

--- src/main/java/org/benedetto/imageranking/FloatUtils.java
+++
@@ -1,9 +0,0 @@
-package org.benedetto.imageranking;
-
-public class FloatUtils {
-
-    public static boolean floatEqual(float first, float second) {
-        return Math.abs(first - second) < 0.0001;
-    }
-
-}
