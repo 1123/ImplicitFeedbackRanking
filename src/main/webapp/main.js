ff5664eb13f686e383140ec7f6e770767c3ee79c
Benedikt Linse <benedikt.linse@gmail.com>
2013-12-15 12:05:14 +0100

    refactored to use pure.js as template rendering

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -16,17 +16,33 @@ function click(userName, password) {
     });
 }
 
+/*
+ * This method populates an html table with images retrieved from the local search web service.
+ * Pure.js is used as a template rendering engine.
+ */
+function fill_image_table(urls) {
+    $('#image_table').append('<table><tr>');
+    var directive = {
+        '.image_link@href':'url',
+        '.image@src':'url'
+    };
+    for (key in urls) {
+        var renderData = { url : urls[key] };
+        var rendered = $('td.template').first().clone().render(renderData, directive);
+        if (key % 3 == 0) $('#image_table').append('</tr><tr>');
+        $('#image_table').append(rendered);
+    }
+    $('#image_table').append('</tr></table>');
+}
+
+/* This method calls the local web service to get a list of image urls for the given search tags */
 function search(number, tag) {
     $.getJSON(
         '/api/graph/search_flickr?number=' + number + '&tag=' + tag,
-        function(urls) {
-            $('#image_list').append('<table><tr>');
-            for (key in urls) {
-                if (key % 3 == 0) $('#image_list').append('</tr><tr>');
-                $('#image_list').append(
-                    '<td><a href="' + urls[key] + '"><img width="100" src=' + urls[key] + '></img></a></td>');
-            }
-            $('#image_list').append('</tr></table>');
-        }
+        function(urls) { fill_image_table(urls); }
     );
 }
+
+$(document).ready(function() {
+    search(18, 'Katze');
+});
