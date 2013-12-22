e3082244e81ea83ca8e994c4658f71eb16c5af5e
Benedikt Linse <benedikt.linse@gmail.com>
2013-12-22 14:04:32 +0100

    making Graph class generic; working on frontend demo

---
+++ test/java/org/benedetto/ifr/MapReduceExample.java
@@ -0,0 +1,11 @@
+package org.benedetto.ifr;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: linse
+ * Date: 12/19/13
+ * Time: 13:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class MapReduceExample {
+}
